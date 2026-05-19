7aad0df775db1abd0e131b602ca8ecadc076c5ca
Matheus9503 <contapessoal9503@gmail.com>
2026-05-19 12:38:07 -0300

    Implementação inicial do PDO
    Vou fazer os includes ainda hoje beleza?

---
+++ Farmacia/config/conexao.php
@@ -0,0 +1,23 @@
+<?php
+
+/* Conexão principal com o banco usando PDO */
+$host = "localhost";
+$banco = "farmacia";
+$usuario = "root";
+$senha = "";
+
+try {
+
+    /* PDO facilita segurança e organização das consultas */
+    $conexao = new PDO(
+        "mysql:host=$host;dbname=$banco;charset=utf8",
+        $usuario,
+        $senha
+    );
+
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $erro) {
+
+    die("Erro na conexão: " . $erro->getMessage());
+}
