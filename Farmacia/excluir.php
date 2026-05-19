960cbc0f6dd09d66fd36514460411d0c55a4ce81
DonNathan <nathan.noobao@gmail.com>
2026-05-19 15:20:10 -0300

    Versão revisada e testada do excluir

--- Farmacia/excluir.php
+++ Farmacia/excluir.php
@@ -2,13 +2,18 @@
 
 require_once "config/conexao.php";
 
+/* Verifica se o ID existe */
+if(!isset($_GET['id'])) {
+    header("Location: index.php");
+    exit;
+}
 $id = $_GET['id'];
-
-/* Exclusão simples do produto selecionado */
-$sql = $conexao->prepare("DELETE FROM produtos WHERE id = :id");
-
+/* Exclusão */
+$sql = $conexao->prepare(
+    "DELETE FROM produtos WHERE id = :id"
+);
 $sql->execute([
     ':id' => $id
 ]);
-
 header("Location: index.php");
+exit;
