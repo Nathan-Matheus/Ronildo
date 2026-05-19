7b03136d636221d2308c7dc173d57ecc1e33986d
Matheus9503 <contapessoal9503@gmail.com>
2026-05-19 14:14:09 -0300

    Exclusão adicionada

---
+++ Farmacia/excluir.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "config/conexao.php";
+
+$id = $_GET['id'];
+
+/* Exclusão simples do produto selecionado */
+$sql = $conexao->prepare("DELETE FROM produtos WHERE id = :id");
+
+$sql->execute([
+    ':id' => $id
+]);
+
+header("Location: index.php");
