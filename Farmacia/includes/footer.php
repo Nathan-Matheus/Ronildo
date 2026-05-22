372ac59fb8d7d05075abc22ea91b0d2e84a0a809
Nathan-G23 <nathangabriel2020p@gmail.com>
2026-05-22 10:29:40 -0300

    Finalização do footer

--- Farmacia/includes/footer.php
+++ Farmacia/includes/footer.php
@@ -1,6 +1,6 @@
     <!-- Rodapé simples usado em todas as páginas -->
     <footer>
-        <p>2026 - Matheus Theodoro e Nathan Nabas</p>
+        <p>2026 - Matheus Theodoro, Nathan Nabas e Nathan Gabriel</p>
     </footer>
 
 </body>
