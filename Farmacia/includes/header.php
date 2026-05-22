87bd466add947c35175d968e92edc907f82fd869
Nathan-G23 <nathangabriel2020p@gmail.com>
2026-05-22 10:31:43 -0300

    Update final header

--- Farmacia/includes/header.php
+++ Farmacia/includes/header.php
@@ -9,13 +9,14 @@
     <!-- Arquivo principal de estilo -->
 
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="images/Logo.png" type="image/png">
 
 </head>
 <body>
 
     <!-- Cabeçalho reutilizado -->
     <header>
-        <h1>💊 FarmaX</h1>
+        <h1> <img src="images/Logo.png" width="100" height="90"></h1>
     </header>
 
     <!-- Navegação principal -->
